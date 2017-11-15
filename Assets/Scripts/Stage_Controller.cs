3aa9605325c81285374990cc729ab5dbe5801af9
大塚芳 <yoshi.gin.4982@gmail.com>
2017-11-15 15:30:56 +0900

    ステージセレクト画面差し替え終了

---
+++ Assets/Scripts/Stage_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Stage_Controller: MonoBehaviour {
+    public float speed = 2;
+    GameObject mainCamera;
+    Vector2 vec;
+    //スクロールカメラ
+    public float camera_scr;
+    public int camera_move_state = 0;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        mainCamera = GameObject.Find("Main Camera");		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {   
+        //　移動処理 
+        if (camera_move_state == 1)
+        {
+            camera_scr -= 0.03f;
+            transform.position = new Vector3(0.0f, camera_scr, -10.0f);
+            if (camera_scr <= -13.5f)
+            {
+                camera_scr = -13.5f;
+                //camera_move_state = 2;
+            }
+        }
+        if (camera_move_state == 2)
+        {
+            camera_scr += 0.03f;
+            transform.position = new Vector3(0.0f, camera_scr, -10.0f);
+            if (camera_scr >= 0)
+            {
+                camera_scr = 0;
+                //camera_move_state = 2;
+                camera_move_state = 0;
+            }
+        }
+	}
+}
