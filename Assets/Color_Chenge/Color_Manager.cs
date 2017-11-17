003cbdf07a1b044797899391ba400cccae79c341
LAPTOP-NTLA8QAG\gso24 <sac77qbc@gmail.com>
2017-11-17 16:02:30 +0900

    no message

---
+++ Assets/Color_Chenge/Color_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color_Manager : MonoBehaviour
+{
+    public GameObject[] color_chenge_prefab = new GameObject[3];
+    public Sprite[] sprite_color = new Sprite[3];
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        color_chenge_prefab[0].GetComponent<SpriteRenderer>().sprite = sprite_color[1];
+	}
+}
