c7974bf2bd93349b0181c66f91bf09fc2fc5877d
LAPTOP-NTLA8QAG\gso24 <sac77qbc@gmail.com>
2017-11-22 22:15:45 +0900

    スイッチギミックを追加しました

---
+++ Assets/Scripts/Renfe_hit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Renfe_hit : MonoBehaviour
+{
+    public List<GameObject> obj = new List<GameObject>();
+    private int coll_count = 0;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "Gimic")
+        {
+            obj.Add(trigger.gameObject);
+            //obj[coll_count] = trigger.gameObject;
+            //coll_count += 1;
+        }
+    }
+}
