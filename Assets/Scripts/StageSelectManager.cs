ff10f6a3f52b8c846731153351d89ff59fc535ad
大塚芳 <yoshi.gin.4982@gmail.com>
2017-12-28 13:56:19 +0900

    UI修正

--- Assets/Scripts/StageSelectManager.cs
+++ Assets/Scripts/StageSelectManager.cs
@@ -52,6 +52,32 @@ public class StageSelectManager : MonoBehaviour
     public bool Debug_Mode = false;
     private bool blackFlag = false;
     private int[] _temp = new int[10];
+    void Awake_1()
+    {
+        // 開発している画面を元に縦横比取得 (縦画面) iPhone6, 6sサイズ
+        //float developAspect = 750.0f / 1334.0f;
+        // 横画面で開発している場合は以下の用に切り替えます
+        float developAspect = 1334.0f / 750.0f;
+
+        // 実機のサイズを取得して、縦横比取得
+        float deviceAspect = (float)Screen.width / (float)Screen.height;
+
+        // 実機と開発画面との対比
+        float scale = deviceAspect / developAspect;
+
+        Camera mainCamera = Camera.main;
+
+        // カメラに設定していたorthographicSizeを実機との対比でスケール
+        float deviceSize = mainCamera.orthographicSize;
+        // scaleの逆数
+        float deviceScale = 1.0f / scale;
+        // orthographicSizeを計算し直す
+        mainCamera.orthographicSize = deviceSize * deviceScale;
+
+    }
+
+
+
     void Awake()
     {
         for (int i = 0; i < transform.childCount; i++)
